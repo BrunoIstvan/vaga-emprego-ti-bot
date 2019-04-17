a271fe404e5ce29a376b4389a7aa24cb62f746e9
fnsilva23 <fn.silva23@gmail.com>
2019-04-16 21:45:42 -0300

    teste 12

--- app.js
+++ app.js
@@ -86,8 +86,25 @@ app.post("/api/vaga/consulta", function (req, res) {
             console.log(todos)
             res.setHeader('Content-Type','application/json')
             res.send(JSON.stringify({
-                "fulfillmentText": 
-             
+                "fulfillmentText": "estamos testando essa porra",
+                    "fulfillmentMessages": [
+                        {
+                        "object(Message)":"alguma coisa"
+                        }
+                    ],
+                    "source": "string",
+                    "payload": {
+                        "object(Message)":"alguma coisa"
+                    },
+                    "outputContexts": [
+                    {
+                        "object(Message)":"alguma coisa"
+                    }
+                    ],
+                    "followupEventInput": {
+                        "object(Message)":"alguma coisa"
+                    },
+
                     // todos.map(v=>(
                     //     `Cargo: ${v.cargo} 
                     //     Nível: ${v.nivel} 
@@ -96,18 +113,18 @@ app.post("/api/vaga/consulta", function (req, res) {
                     //     Salário: R$${v.salario.toFixed(2)} 
                     //     Link da vaga: ${v.link}`
                     // ))
-                    todos.map(v => {
-                        var array = []
-                        var string = `Cargo: ${v.cargo} 
-                             Nível: ${v.nivel} 
-                             Cidade: ${v.cidade}
-                             Empresa: ${v.nomeEmpresa}
-                             Salário: R$${v.salario.toFixed(2)} 
-                             Link da vaga: ${v.link},`
-                        array.push(string)
-                        array.toString().replace(',', '\n')
-                        return array
-                    })              
+                    // todos.map(v => {
+                    //     var array = []
+                    //     var string = `Cargo: ${v.cargo} 
+                    //          Nível: ${v.nivel} 
+                    //          Cidade: ${v.cidade}
+                    //          Empresa: ${v.nomeEmpresa}
+                    //          Salário: R$${v.salario.toFixed(2)} 
+                    //          Link da vaga: ${v.link},`
+                    //     array.push(string)
+                    //     array.toString().replace(',', '\n')
+                    //     return array
+                    // })              
                 
             }))
         }
